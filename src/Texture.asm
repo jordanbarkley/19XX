0e1825360363cb07b062dee9f256b14baee79231
Jordan <jordan@charloe.com>
2018-06-13 22:15:48 -0400

    modifed Texture.allocate_ parameters

--- src/Texture.asm
+++ src/Texture.asm
@@ -16,33 +16,29 @@ scope Texture {
     // @ Description
     // Wrapper to allocate texture
     // @ Arguments
-    // a0 - texture struct
+    // a0 - width
+    // a1 - height
+    // a2 - ROM address
     // @ Returns
     // v0 - address or OS.NULL if there is not enough space
     scope allocate_: {
-        addiu   sp, sp,-0x0010              // allocate stack space
-        sw      t0, 0x0000(sp)              // ~
-        sw      t1, 0x0004(sp)              // ~
-        sw      ra, 0x0008(sp)              // save registers
+        addiu   sp, sp,-0x0008              // allocate stack space
+        sw      ra, 0x0004(sp)              // save registers
 
         // calculate nBytes
-        lw      t0, 0x0000(a0)              // t0 = width
-        lw      t1, 0x0004(a0)              // t1 = height
-        multu   t0, t1                      // ~
+        multu   a0, a1                      // ~
         mflo    a1                          // a1 = width * height
         sll     a1, a1, 0x0001              // a1 = width * height * sizeof(rgba5551)
 
         // attempt allocate memory
-        lw      a0, 0x000C(a0)              // a0 - ROM address
+        move    a0, a2                      // a0 - ROM address
 //      move    a1, a1                      // a1 - size
         jal     Memory.allococate_rom_
         nop
 
         // return
-        lw      t0, 0x0000(sp)              // ~
-        lw      t1, 0x0004(sp)              // ~
-        lw      ra, 0x0008(sp)              // restore registers
-        addiu   sp, sp, 0x0010              // deallocate stack space
+        lw      ra, 0x0004(sp)              // restore registers
+        addiu   sp, sp, 0x0008              // deallocate stack space
         jr      ra                          // return
         nop
     }
